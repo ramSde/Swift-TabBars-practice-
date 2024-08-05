43a0a22ce39da36982a21f1923c2f82ece64b525
ramSde <techmacchu@gmail.com>
2024-08-05 11:08:30 +0530

    first commit

---
+++ create Tab Bar Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  create Tab Bar Test
+//
+//  Created by Zignuts Tchnolab on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
